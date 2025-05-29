6d99b79ce1e06e838910dd6ac0ef3dc987344423
Bread <764456+joshcartme@users.noreply.github.com>
2025-05-29 16:57:48 -0700

    make lib.js the output from tsgo

--- lib.js
+++ lib.js
@@ -1,1 +1,5 @@
-export const foo = "foo";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getRand = void 0;
+const getRand = () => Math.random();
+exports.getRand = getRand;
